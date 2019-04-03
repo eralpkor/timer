c6aed7ccc7a267a61d2208e63649059abf3ec591
eralpkor <eralp.ny@gmail.com>
2019-04-02 23:24:10 -0400

    made it more responsive for small screens

--- js/backup.js
+++ js/backup.js
@@ -3,25 +3,60 @@ var seconds = 30,
     title = document.getElementsByTagName('title')[0],
     session = document.getElementById('countdown'),
     buttons = document.querySelectorAll('[data-time]'),
+    manualTimer = document.getElementById('manual-timer'),
     alarmSound = new Audio('http://soundbible.com/mp3/Zen Buddhist Temple Bell-SoundBible.com-331362457.mp3'),
     beep1 = new Audio('http://soundbible.com/mp3/Bleep-SoundBible.com-1927126940.mp3'),
     beep2 = new Audio('http://soundbible.com/mp3/Beep-SoundBible.com-923660219.mp3')
 
 // Stop countdown when it reaches to 0. Run countdown and update the DOM
 function counter(seconds) {
-  this.timerId = setInterval(function() {
+  timerId = setInterval(function() {
     if (this.seconds < 0) {
       this.seconds = 0;
     }
     var secondsLeft = this.seconds--;
     
     if (secondsLeft < 1) {
-      alarmSound.play();
+      // alarmSound.play();
       clearInterval(timerId);
     }
     
     displayTimeLeft(secondsLeft);
   }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  var minutes = Math.floor(seconds / 60);
+  var remainderSeconds = seconds % 60;
+  var display = `${minutes}:${remainderSeconds < 10 ? "0" : ""}${remainderSeconds}`;
+  document.title = display;
+  session.textContent = display;
+}
+
+function custom() {
+  document.customForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    clearInterval(timerId);
+    seconds = manualTimer.value;
+    console.log(parseInt(seconds));
+    counter(seconds);
+  });
+}
+
+
+// Listen on buttons for manual set time.
+buttons.forEach(function(button) {
+  button.addEventListener('click', function(e) {
+    var customSeconds = parseInt(e.target.dataset.time);
+    seconds = customSeconds;
+    startTimer(customSeconds);
+  })
+});
+// function that I will add as a break timer later...
+function breakTime(breakSeconds) {
+  clearInterval(timerId);
+  var breakSeconds = 300;
+  counter(breakSeconds);
 }
 
 function startTimer() {
@@ -35,37 +70,16 @@ function stopTimer() {
   // beep2.play();
 }
 
-function reset(time) {
-  return time;
-}
 // Need to clear interval to reset timer
 function resetTimer() {
+  console.log('Reset button pressed');
   clearInterval(timerId);
-  seconds = 30;
+  seconds = custom();
+  console.log(seconds);
   // document.getElementById('startButton').innerText = 'Start';
   displayTimeLeft(seconds);
+  console.log(displayTimeLeft(seconds));
   title.innerHTML = '30 Second Timer';
 }
 
-function displayTimeLeft(seconds) {
-  var minutes = Math.floor(seconds / 60);
-  var remainderSeconds = seconds % 60;
-  var display = `${minutes}:${remainderSeconds < 10 ? "0" : ""}${remainderSeconds}`;
-  document.title = display;
-  session.textContent = display;
-}
-
-buttons.forEach(function(button) {
-  button.addEventListener('click', function(e) {
-    seconds = parseInt(e.target.dataset.time)
-    // reset(seconds);
-    startTimer(seconds);
-    console.log(parseInt(seconds));
-  })
-});
-// function that I will add as a break timer later...
-function breakTime(breakSeconds) {
-  clearInterval(timerId);
-  var breakSeconds = 300;
-  counter(breakSeconds);
-}
+custom();
